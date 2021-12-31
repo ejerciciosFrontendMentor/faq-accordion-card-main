dd80ce7d0db21ab1b837fc37a45accd37df53251
Jose Esteves <josefelixestevenpacheco@gmail.com>
2021-12-31 17:35:28 -0400

    version de movil y escritorio, acordion echo falta la caja

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const accordeon = document.getElementById("contentAccordeon");
+accordeon.addEventListener("click", (e) => {
+  if (e.target.classList.contains("accordeon")) {
+    e.target.firstElementChild.classList.toggle(
+      "accordeon__title--show"
+    );
+    e.target.lastElementChild.classList.toggle(
+      "accordeon__cotentText--show"
+    );
+  }
+  e.stopPropagation();
+});
