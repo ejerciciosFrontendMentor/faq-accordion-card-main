a9540cef575b63c22b6247007be4df20564448a2
Jose Esteves <josefelixestevenpacheco@gmail.com>
2022-01-02 17:04:10 -0400

    acordeon echo

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
+/** @format */
 const accordeon = document.getElementById('contentAccordeon');
 accordeon.addEventListener('click', e => {
 	if (e.target.classList.contains('accordeon')) {
-    const accordeonAll = accordeon.querySelectorAll('.accordeon');
-      accordeonAll.forEach(item=>{
-        if(item.firstElementChild.classList.contains('accordeon__title--show') && item.lastElementChild.classList.contains('accordeon__cotentText--show')){
-          item.firstElementChild.classList.remove('accordeon__title--show')
-          item.lastElementChild.classList.remove('accordeon__cotentText--show')
-        }
-      })  
-      e.target.firstElementChild.classList.toggle('accordeon__title--show');
-      e.target.lastElementChild.classList.toggle('accordeon__cotentText--show');
-    }
+		e.target.firstElementChild.classList.toggle('accordeon__title--show');
+		e.target.lastElementChild.classList.toggle('accordeon__cotentText--show');
+  }
 	e.stopPropagation();
 });
